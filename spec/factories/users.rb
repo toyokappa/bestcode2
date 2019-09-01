a3cbe920ef7540a90f7740ee9ee1571fa42adc44
Yuta Toyokawa <kppg42@gmail.com>
2019-09-02 01:07:04 +0900

    create user

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    provider { "MyString" }
+    uid { "MyString" }
+    name { "MyString" }
+    display_name { "MyString" }
+    email { "MyString" }
+    access_token { "MyString" }
+    image_url { "MyString" }
+  end
+end
