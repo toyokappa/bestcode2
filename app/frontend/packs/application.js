4770761e1624f422eee24db577149d74e5f1be33
Yuta Toyokawa <kppg42@gmail.com>
2019-09-14 11:31:55 +0900

    setup fontawesome

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,6 +2,7 @@ import Rails from "rails-ujs";
 import Turbolinks from "turbolinks";
 
 import "bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
 import "../stylesheets/application.scss";
 
 function init() {
