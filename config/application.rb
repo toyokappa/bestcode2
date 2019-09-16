05282ac7e06b8769f10e5b17c51f2ad1c4469f39
Yuta Toyokawa <kppg42@gmail.com>
2019-09-16 20:59:37 +0900

    set i18n for plan form

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Bestcode2
 
     config.active_record.default_timezone = :local
     config.time_zone = "Tokyo"
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :ja
   end
 end
