cb394ba32931ef388e61806a50cb0428bec35c82
Yuta Toyokawa <kppg42@gmail.com>
2019-10-02 22:31:16 +0900

    create messages

--- app/controllers/users/messages_controller.rb
+++ app/controllers/users/messages_controller.rb
@@ -1,10 +1,42 @@
 class Users::MessagesController < Users::ApplicationController
+  before_action :find_message_user
+  before_action :list_messages
+
   def index
+    @message = current_user.sent_messages.build
+
+    # 既読処理
+    received_messages = @messages.where(receiver: current_user)
+    received_messages.update_all(is_already_read: true) unless received_messages.all?(:is_already_read?)
   end
 
   def create
+    @message = current_user.sent_messages.build(message_params)
+    @message.receiver = @message_user
+    if @message.save
+      redirect_to users_messages_path(@message_user.name), success: "メッセージを送信しました"
+    else
+      render :index
+    end
   end
 
-  def update
+  def destroy
+    deletable_messages = @messages.where(sender: current_user)
+    deletable_messages.find(params[:id]).destroy!
+    redirect_to users_messages_path(@message_user.name), success: "メッセージを削除しました"
   end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:body)
+    end
+
+    def find_message_user
+      @message_user = User.find_by!(name: params[:user_name])
+    end
+
+    def list_messages
+      @messages = Message.exchange_between(current_user, @message_user).order(created_at: :desc).includes(:sender)
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  scope :exchange_between, ->(user, another_user) {
+    massagers = [user, another_user]
+    where(sender: massagers, receiver: massagers)
+  }
+
+  delegate :name, to: :sender, prefix: :sender
+  validates :body, presence: true, length: { maximum: 5000, allow_nil: true }
 end
