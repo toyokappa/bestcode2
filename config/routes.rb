d8bc9f79305ba6a3620eeb906d71ca86b73bfdf6
Yuta Toyokawa <kppg42@gmail.com>
2019-10-04 19:31:21 +0900

    configure routes for profile

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
     # NOTE: GitHubのnameをUser#nameとして使用しているので一旦問題ないが
     #       自分でnameを変更できるようにする場合、文字種を気をつける必要がある
     resources :messages, only: %i[index create destroy], path: "/:user_name/messages"
+    resource :profile, only: %i[edit, update]
   end
 
+  get "/:user_name/profile", to: "profiles#show", as: :profile
   resources :plans, only: %i[show]
 end
