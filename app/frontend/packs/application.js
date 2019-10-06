58d8e391312867320684a6734a2d93ceccb5a98a
Yuta Toyokawa <kppg42@gmail.com>
2019-10-06 21:35:37 +0900

    add toggle form field function

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,6 +5,8 @@ import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "../stylesheets/application.sass";
 
+import "../javascripts/utils/toggle_form_field.ts";
+
 function init() {
   Rails.start();
   Turbolinks.start();
