12f4ebfcc6ebc35c0b18a803336c09d469a7d185
Yuta Toyokawa <kppg42@gmail.com>
2019-10-14 22:00:01 +0900

    add validation spec to plan and course models

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Course, type: :model do
+  describe "validation" do
+    describe "#name" do
+      let(:course) { build :course, name: nil }
+      before { course.valid? }
+
+      it "必須である" do
+        expect(course.errors[:name]).to include("を入力してください")
+      end
+    end
+  end
+
   describe ".with_alive_contracts" do
     let(:user) { create :user }
     let(:plan) { create :plan }
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Plan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+
+    describe "#name" do
+      let(:plan) { build :plan, name: nil }
+      before { plan.valid? }
+
+      it "必須である" do
+        expect(plan.errors[:name]).to include("を入力してください")
+      end
+    end
+  end
 end
