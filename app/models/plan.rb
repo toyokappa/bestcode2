8daafa7ed697f99b9b373b9a45a15462299d54ea
Yuta Toyokawa <kppg42@gmail.com>
2019-10-24 23:29:09 +0900

    refactor plan form with accepts_nested_attributes_for

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,7 @@
 class Plan < ApplicationRecord
   belongs_to :user
   has_many :courses, dependent: :destroy
+  accepts_nested_attributes_for :courses, allow_destroy: true
   enum state: { draft: 0, published: 1 }, _prefix: true
 
   validates :name, presence: true
