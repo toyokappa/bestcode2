328232e510d2649769422680667e371686e5cddf
Yuta Toyokawa <kppg42@gmail.com>
2019-10-26 06:14:19 +0900

    add plan search logic

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -6,4 +6,16 @@ class Plan < ApplicationRecord
 
   validates :name, presence: true
   validates :courses, presence: { message: "を最低1件は登録してください" }
+
+  scope :search, ->(keyword = nil) do
+    return all if keyword.blank? # キーワードが無ければ全件返す
+
+    sql = <<~"SQL"
+      plans.name LIKE :keyword OR
+      plans.description LIKE :keyword OR
+      courses.name LIKE :keyword OR
+      courses.description LIKE :keyword
+    SQL
+    eager_load(:courses).where(sql, keyword: "%#{keyword}%")
+  end
 end
