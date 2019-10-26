04d14b5f1ee59915dacb3636a544c0e1934cbf71
Yuta Toyokawa <kppg42@gmail.com>
2019-10-27 00:12:11 +0900

    add test case for plan search

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -11,4 +11,38 @@ RSpec.describe Plan, type: :model do
       end
     end
   end
+
+  describe "scope" do
+    describe ".search" do
+      let!(:plan1) { create :plan, name: "hoge", description: "fuga" }
+      let!(:plan2) { create :plan, name: "xxxx", description: "yyyy" }
+      let!(:plan3) { create :plan, name: "oraora", description: "mudamuda" }
+
+      before do
+        create :course, plan: plan1, name: "aaaa", description: "bbbb"
+        create :course, plan: plan2, name: "hogehoge", description: "piyopiyo"
+      end
+
+      context "hogeというキーワードを入力した場合" do
+        it "2件のプランが返される" do
+          plans = Plan.search("hoge")
+          expect(plans.count).to eq 2
+        end
+      end
+
+      context "zzzzというキーワードを入力した場合" do
+        it "1件のプランが返される" do
+          plans = Plan.search("zzzz")
+          expect(plans.count).to eq 0
+        end
+      end
+
+      context "キーワードを入力しなかった場合" do
+        it "すべてのプランが返される" do
+          plans = Plan.search
+          expect(plans.count).to eq 3
+        end
+      end
+    end
+  end
 end
