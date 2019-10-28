5ea092047e6cf5f3221d5fcdd661a7aafbbf5a2e
Yuta Toyokawa <kppg42@gmail.com>
2019-10-28 21:04:23 +0900

    create plan search spec

---
+++ spec/system/plan_search_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe "PlanSearch", type: :system do
+  let!(:plan1) { create :plan, name: "リモートワークでの働き方を伝授" }
+  let!(:plan2) { create :plan, name: "Railsエンジニアに転職する方法教えます", description: "私はリモートで働いています。" }
+  let!(:plan3) { create :plan, name: "エンジニアとして即戦力で働く方法をレクチャーします" }
+  let!(:plan4) { create :plan, name: "副業で月5万円稼ぐための学習サポート" }
+
+  before do
+    create_list :plan, 6
+    create :course, plan: plan2
+    create :course, plan: plan3, name: "リモートコース"
+    create :course, plan: plan4, description: "リモートによるビデオチャットでの受講となります"
+    visit root_path
+    fill_in "keyword", with: "#{keyword}\n"
+  end
+
+  context "キーワードを入力して検索を行った場合" do
+    let(:keyword) { "リモート" }
+
+    it "該当の検索件数が表示される" do
+      expect(page).to have_content "4人のメンターが見つかりました"
+    end
+
+    it "該当のプランが表示される" do
+      expect(page).to have_content "リモートワークでの働き方を伝授"
+      expect(page).to have_content "Railsエンジニアに転職する方法教えます"
+      expect(page).to have_content "エンジニアとして即戦力で働く方法をレクチャーします"
+      expect(page).to have_content "副業で月5万円稼ぐための学習サポート"
+    end
+  end
+
+  context "キーワードを入力せずに検索を行った場合" do
+    let(:keyword) { nil }
+
+    it "プランの全件数が表示される" do
+      expect(page).to have_content "10人のメンターが見つかりました"
+    end
+  end
+end
